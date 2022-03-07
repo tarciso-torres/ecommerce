797d70e759a2b30beb7eb192c6006e062fcd7b23
Tarciso Torres <tarcisoftorres@gmail.com>
2022-03-07 09:11:15 -0300

    Initialize project

---
+++ src/main/java/com/redfort/ecommerce/NewOrderMain.java
@@ -0,0 +1,39 @@
+package com.redfort.ecommerce;
+
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+
+public class NewOrderMain {
+
+    public static void main(String args[]) throws ExecutionException, InterruptedException {
+        var producer = new KafkaProducer<String, String>(properties());
+        var key = UUID.randomUUID().toString();
+        var value = "121212,67523,98709870931";
+        var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", key, value);
+        Callback callback = (data, ex) -> {
+            if(ex != null) {
+                ex.printStackTrace();
+            }
+            System.out.println("sucesso enviando " + data.topic() + ":::partition" + data.partition() + "/ offset" + data.offset() + "/timestamp " + data.timestamp());
+        };
+        var email = "Thank for you order! We are processing your order!";
+        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", key, email);
+        producer.send(record, callback).get();
+        producer.send(emailRecord, callback).get();
+    }
+
+    private static Properties properties() {
+        var properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        return properties;
+    }
+}
