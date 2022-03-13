2c38018e34388e1e17181ef5572efd63c3847822
Tarciso Torres <tarcisoftorres@gmail.com>
2022-03-13 14:26:58 -0300

    Serialization with GSON

--- src/main/java/com/redfort/ecommerce/NewOrderMain.java
+++ src/main/java/com/redfort/ecommerce/NewOrderMain.java
@@ -1,21 +1,27 @@
 package com.redfort.ecommerce;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
 
     public static void main(String args[]) throws ExecutionException, InterruptedException, IOException {
-        try(var dispatcher = new KafkaDispatcher()){
-            for(var i = 0; i < 10; i++) {
+        try(var orderDispatcher = new KafkaDispatcher<Order>()) {
+            try (var emailDispatcher = new KafkaDispatcher<String>()) {
+                for (var i = 0; i < 10; i++) {
 
-                var key = UUID.randomUUID().toString();
-                var value = key + ",67523,98709870931";
-                dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
+                    var userId = UUID.randomUUID().toString();
+                    var orderId = UUID.randomUUID().toString();
+                    var amount = new BigDecimal(Math.random() * 5000 + 1);
+                    var order = new Order(userId, orderId, amount);
 
-                var email = "Thank for you order! We are processing your order!";
-                dispatcher.send("ECOMMERCE_SEND_EMAIL", key, value);
+                    orderDispatcher.send("ECOMMERCE_NEW_ORDER", userId, order);
+
+                    var email = "Thank for you order! We are processing your order!";
+                    emailDispatcher.send("ECOMMERCE_SEND_EMAIL", userId, email);
+                }
             }
         }
     }
