c6b2330731c62738a0422b8debe4fe4d0b01ed47
Tarciso Torres <tarcisoftorres@gmail.com>
2022-03-14 09:26:51 -0300

    log migration

--- src/main/java/com/redfort/ecommerce/KafkaService.java
+++ src/main/java/com/redfort/ecommerce/KafkaService.java
@@ -9,6 +9,7 @@ import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
 import java.util.UUID;
+import java.util.regex.Pattern;
 
 class KafkaService implements Closeable {
 
@@ -16,9 +17,18 @@ class KafkaService implements Closeable {
     private final ConsumerFunction parse;
 
     public KafkaService(String groupId, String topic, ConsumerFunction parse){
+        this(parse, groupId);
+        consumer.subscribe(Collections.singletonList(topic));
+    }
+
+    public KafkaService(String groupId, Pattern topic, ConsumerFunction parse) {
+        this(parse, groupId);
+        consumer.subscribe(topic);
+    }
+
+    private KafkaService(ConsumerFunction parse, String groupId) {
         this.parse = parse;
         this.consumer = new KafkaConsumer<>(properties(groupId));
-        consumer.subscribe(Collections.singletonList(topic));
     }
 
     void rum() {
